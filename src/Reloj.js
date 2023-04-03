3591b475cfcb7eec375c8be0ad7c1a772b42a059
Beverly M <bevponi@gmail.com>
2023-04-03 20:40:41 +0000

    Empezando

---
+++ src/Reloj.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import React, { useState } from 'react';
+
+function Clock() {
+  // Declara una nueva variable de estado, que llamaremos "count".
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+            
+      
+    </div>
+  );
+}
