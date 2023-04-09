3ee78968b5e49a820cd180c9a178fc9f95b7b2a4
Beverly M <bevponi@gmail.com>
2023-04-09 21:29:48 +0000

    Falta icono y detalles

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import { prototype } from "file-loader";
+import propTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -12,6 +14,7 @@ function SimpleCounter(props) {
         <div className="bigCounter">
             <div className="calendar">
                 <i className="bi bi-clock"></i></div>
+
             <div className="four">{props.digitFour}</div>
             <div className="three">{props.digitThree}</div>
             <div className="two">{props.digitTwo}</div>
@@ -19,8 +22,26 @@ function SimpleCounter(props) {
 
         </div>
     )
-
-
 }
 
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.prototypes = {
+    digitFour: PropTypes.number,
+    digitThree: prototypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(function () {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const Two = Math.floor(counter / 10);
+    const One = Math.floor(counter / 1);
+
+    counter++;
+
+    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={Two} digitThree={Three} digitFour={Four} />, document.querySelector("#app"));
+
+}, 1000);
+
+
