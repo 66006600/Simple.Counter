1b873bb71c581a24cd36667ad36ccd6f0205d373
Beverly M <bevponi@gmail.com>
2023-04-10 21:34:41 +0000

    Afregar estilos

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,7 @@
-import { prototype } from "file-loader";
 import PropTypes from "prop-types";
 import React from "react";
-import ReactDOM from "react-dom";
-
+import ReactDOM from "react-dom"; 
 // include your styles into the webpack bundle
-
-
 
 //render your react application
 function SimpleCounter(props) {
@@ -35,12 +31,12 @@ let counter = 0;
 setInterval(function () {
     const four = Math.floor(counter / 1000);
     const three = Math.floor(counter / 100);
-    const Two = Math.floor(counter / 10);
-    const One = Math.floor(counter / 1);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
 
     counter++;
 
-    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={Two} digitThree={Three} digitFour={Four} />, document.querySelector("#app"));
+    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app"));
 
 }, 1000);
 
